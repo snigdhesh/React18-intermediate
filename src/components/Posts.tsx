5ed9b6cb08456626ec32f0ef0cac391f11eb8cf4
snigdhesh <snigdhesh@live.com>
2024-04-05 19:00:20 -0400

    Error handling with useQuery hook

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -8,18 +8,21 @@ interface Post {
 
 function Posts() {
     const fetchPosts = () => {
-        return axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+        return axios.get<Post[]>("https://xjsonplaceholder.typicode.com/posts")
             .then(res => res.data)
     }
 
-    const { data } = useQuery({
+    const { data, error } = useQuery<Post[], Error>({
         queryKey: ['posts'],
         queryFn: fetchPosts
     })
+
+    if (error) return <p className="text-danger h1">{error.message}</p>
+
     return (<>
         <div className="container mt-5">
             <ul className="list-group">
-                {data?.map(post=> <li className="list-group-item" key={post.id}>{post.title}</li>)}
+                {data?.map(post => <li className="list-group-item" key={post.id}>{post.title}</li>)}
             </ul>
         </div>
     </>)
