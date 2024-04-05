105557c701fd9cfe8ba8ac9cea876196758046d2
snigdhesh <snigdhesh@live.com>
2024-04-05 18:51:09 -0400

    Installed bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const client = new QueryClient();
