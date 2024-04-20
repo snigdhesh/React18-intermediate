4131eca68fbaade624b66dfdbbc92be3b6feef0f
snigdhesh <snigdhesh@live.com>
2024-04-20 08:34:19 -0400

    Routing: Get info about current route/path

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,11 +1,12 @@
-import { useParams, useSearchParams } from "react-router-dom"
+import { useLocation, useParams, useSearchParams } from "react-router-dom"
 
 const UserDetailPage = () => {
     const params = useParams();
     const [searchParams, setSearchParams] = useSearchParams(); //With this we can access query string prams
     console.log(searchParams.toString()) //We need this to get queryparam data. Log searchPrams object and see what it offers.
     //Note: setSearchParams will have side effect, so this should be called only in event handlers or useEffect.
-    
+    const location = useLocation(); //This hook gives metadata/info about the current route/path
+    console.log(location) 
   return (
     <div>
         <p>User {params.id} </p>
