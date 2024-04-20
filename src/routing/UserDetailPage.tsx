145ae8e3fafcd5abbdbfc07794779d21f1d6d9bf
snigdhesh <snigdhesh@live.com>
2024-04-20 08:32:54 -0400

    Routing: Capture path param and query param data

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,7 +1,17 @@
+import { useParams, useSearchParams } from "react-router-dom"
 
 const UserDetailPage = () => {
+    const params = useParams();
+    const [searchParams, setSearchParams] = useSearchParams(); //With this we can access query string prams
+    console.log(searchParams.toString()) //We need this to get queryparam data. Log searchPrams object and see what it offers.
+    //Note: setSearchParams will have side effect, so this should be called only in event handlers or useEffect.
+    
   return (
-    <div>User</div>
+    <div>
+        <p>User {params.id} </p>
+        <p>Query params : {searchParams.toString()}</p> {/*Try something like http://localhost:5173/users/2?age=27&height=7*/}
+    </div>
+
   )
 }
 
